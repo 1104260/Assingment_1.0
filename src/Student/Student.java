d2013c2e84e031d3e6acd971f349909e7324f308
1104620 <1104260@stud.edu.uot.pk>
2021-04-02 12:03:37 +0500

    Student CLass

---
+++ src/Student/Student.java
@@ -0,0 +1,25 @@
+package Student;
+
+public class Student {
+
+    public static void main(String[] args) {
+        System.out.println(" Im bad programmer ");
+        StudentName BS=new StudentName();
+        BS.Insert("Jamal");
+        BS.Insert("Tayyab");
+        BS.Insert("Kasum");
+        BS.Insert("Imtiaz Ali");
+        BS.Insert("Faisal");
+        BS.Insert("Junaid");
+        BS.Insert("Noman");
+        BS.Insert("Imdad");
+        BS.Insert("Kareem");
+        BS.Insert("Hameed");
+        
+        
+        BS.Search("Noman");
+        BS.Delete(6);
+        BS.Search("Noman");
+    }
+    
+}
